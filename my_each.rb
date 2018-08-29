33ce0f41d80ec85acc5f79e8c350949b9129399b
zhifengou@hotmail.com <zhifengou@hotmail.com>
2018-08-29 19:48:27 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(arr)
+  if block_given?
+  i=0
+  while i<arr.size 
+  yield arr[i]
+  i+=1
+  end
+  return arr
+else
+  "No block was given"
+end
 end
